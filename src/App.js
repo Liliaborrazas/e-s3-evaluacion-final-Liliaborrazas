63f72c3e3f9d1b38ac59527b92861533d7113b67
lilia <liliaborrazas14@gmail.com>
2019-01-24 12:22:21 +0100

    filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterList from './components/CharacterList';
+
+const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state={ 
+      harry:[],
+      filter: ""
+      
+    }
+  this.filterInput=this.filterInput.bind(this); 
+  this.filterPerson=this.filterPerson.bind(this); 
+  };
+  
+  componentDidMount() {
+    fetch(ENDPOINT)
+     .then(response => response.json())
+      .then(data=>{
+        const dataHarry = data.map((item, index)=>{
+          return{...item, id:index};
+        });
+        this.setState({harry:dataHarry})
+  }); 
+};
+
+  filterPerson(e){
+    const author = e.currentTarget.value;
+    this.setState({filter:author});
+
+
   }
-}
+  filterInput(){
+    const harry = this.state.harry;
+    const filter = this.state.filter;
+    return harry.filter(item=>
+      item.name.includes(filter)
+
+    )
+
+  }
+  render() {
+     return (
+       <div className="app">
+       <header>
+         <div>
+           <h1>Harry Potter characters</h1>
+           <input type="text" className="input" placeholder="escribe tu personaje" onKeyUp={this.filterPerson}/>
+         </div>
+       </header>
+       <main>
+         <CharacterList filterInput={this.filterInput()}/>
+       </main>
+       </div>
+     );
+   }
+ };
+
 
 export default App;
---
+++ src/components/CharacterList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CharacterList extends Component{
+    render(){
+        
+        return(<ul className="card-harry">
+        {this.props.filterInput.map((item, index)=>{
+          return(<li className="card-item" key={index}>
+          <div className="card-harry_list">
+          <h4>{item.name}</h4>
+          <img src={item.image} alt={item.name}/>
+          <p className="houseItem">{item.house}</p>
+          </div>
+        </li>)
+        })}
+        </ul>)
+
+    }
+}
+export default CharacterList;
