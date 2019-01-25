7af4d675a030a989984bdb58d40becd0295aa967
lilia <liliaborrazas14@gmail.com>
2019-01-25 12:27:58 +0100

    create component CharacterCard

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
 
 class CharacterList extends Component {
     render(){
-        return(<ul className="list-person">
+      return(
+      <ul className="list-person">
         {this.props.filterInput.map((item, index)=>{
-      return(<li className="card-item" key={index}>
-      <div className="card-harry_list">
-      <h4>{item.name}</h4>
-      <img src={item.image} alt={item.name}/>
-      <p className="houseItem">{item.house}</p>
-      </div>
-    </li>)
-    })}
-    </ul>)
+         return(
+           <li className="card-item" key={index}>
+            <CharacterCard item={item}/>
+           </li>)
+        })}
+      </ul>)
 
     }
 }
