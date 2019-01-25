886baa8e1c081159008a307ac41505e8bca7cbd2
lilia <liliaborrazas14@gmail.com>
2019-01-25 15:00:54 +0100

    github pages

---
+++ docs/precache-manifest.3aeb279ec9d6f2e0c65471606fe5646d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fbd32c9f8ede9ce5a0bf",
+    "url": "./static/js/main.fbd32c9f.chunk.js"
+  },
+  {
+    "revision": "3f0be990edb634e44a8b",
+    "url": "./static/js/1.3f0be990.chunk.js"
+  },
+  {
+    "revision": "fbd32c9f8ede9ce5a0bf",
+    "url": "./static/css/main.f6d0d40f.chunk.css"
+  },
+  {
+    "revision": "cfd34562b33af5e3a665cf89590b8c55",
+    "url": "./index.html"
+  }
+];
