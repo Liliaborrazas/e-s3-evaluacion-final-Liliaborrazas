c91de4a1a641a8fb8311264e5404556d2904b68e
lilia <liliaborrazas14@gmail.com>
2019-01-25 09:31:11 +0100

    test fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
+
+
 class App extends Component {
+  constructor(props){
+    super(props)
+  };
+
+  componentDidMount(){
+    fetch(ENDPOINT)
+    .then(response=> response.json())
+    .then(data=>{
+      console.log(data)
+    })
+
+  }
   render() {
     return (
-      <div className="App">
+      <div className="app">
         
       </div>
     );
