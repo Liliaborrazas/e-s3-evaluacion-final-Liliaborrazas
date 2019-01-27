9d1412ce11e85e1f9843d105dabfe846d05890df
lilia <liliaborrazas14@gmail.com>
2019-01-27 15:48:53 +0100

    style card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,9 @@ class CharacterCard extends Component {
     render(){
       return(
         <div className="card-harry_list">
-        <img className="card-img" src={this.props.item.image} alt={this.props.item.name}/>
+        <div className="image-card">
+        <img className="card-img" src={this.props.item.image} alt={this.props.item.name}/>   
+        </div>
         <h4 className="card-name">{this.props.item.name}</h4>
         <p className="card-house">{this.props.item.house}</p>
        </div>
