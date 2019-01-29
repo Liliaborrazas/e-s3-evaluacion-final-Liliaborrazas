b673c1b5e1d1e0125352a800b695a5d980d9a014
lilia <liliaborrazas14@gmail.com>
2019-01-29 08:54:19 +0100

    filter house

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ class App extends Component {
     super(props);
     this.state = {
       harry:[],
-      filter: ""
-    }
+      filter: "",
+      house: ""
+    };
     this.filterInput=this.filterInput.bind(this); 
     this.filterPerson=this.filterPerson.bind(this);
-    
+    this.filterHouse=this.filterHouse.bind(this);
   }
 
   componentDidMount(){
-    fetchHarry
+    fetchHarry()
     .then(data=>{
        const dataHarry = data.map((item, index)=>{
          return {...item, id: index };
@@ -35,9 +36,27 @@ class App extends Component {
       const filter = this.state.filter;
       return harry.filter(item=>
         item.name.toLowerCase().includes(filter.toLowerCase())
-      )
+      );
     }
      
+  filterHouse(e){
+    const newHouse = e.currentTarget.value;
+    this.setState({house:newHouse});
+  }
+
+  filterInputHouse(){
+    const harry = this.state.harry;
+    const house = this.state.house;
+    if(house === 'no'){
+      return harry.filter(item=>
+        item.house === "");
+    }else{
+      return harry.filter(item =>
+        item.house.toUpperCase().includes(house.toUpperCase())
+        
+      );
+    }
+  }
   render() {
     return (
       <div className="app">
@@ -45,11 +64,12 @@ class App extends Component {
           <div className="input-page">
            <h1 className="title-page">Harry Potter characters</h1>
            <input type="text" className="input" placeholder="Escribe tu personaje" onKeyUp={this.filterPerson}/>
+           <input type="text" className="inputHouse" placeholder="Escribe tu casa" onKeyUp={this.filterHouse}/>
          </div>
         </header>
         <main>
           <div className="character-list">
-            <CharacterList filterInput={this.filterInput()}/>
+            <CharacterList filterInput={this.filterInputHouse()} />
           </div>
         </main>
       </div>
--- src/services/harryService.js
+++ src/services/harryService.js
@@ -1,6 +1,6 @@
 const ENDPOINT = 'https://hp-api.herokuapp.com/api/characters';
 
-const fetchHarry = fetch(ENDPOINT).then(response=> response.json());
+const fetchHarry = () => fetch(ENDPOINT).then(response=> response.json());
 
 export { fetchHarry }
 
